7e57d176ab306a06e409d3c4896b5c7d6d7ed185
Vinay-Partap <bhardwajvinay400@gmail.com>
2026-09-12 16:54:58 +0530

    server port configuration

--- src/index.js
+++ src/index.js
@@ -6,4 +6,13 @@ import connectDB from "./db/index.js";
 import express from "express";
 const app = express();
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  });
+})
+.catch((error) => {
+  console.error("Failed to connect to the database:", error);
+  process.exit(1); // Exit the process with an error code
+});
